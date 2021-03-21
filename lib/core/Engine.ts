4bce2772fd9df98af8a1361cede9ad08e48821d6
Adam Josefus <adamjosefus@gmail.com>
2021-03-21 19:09:08 +0100

    Typo

--- lib/core/Engine.ts
+++ lib/core/Engine.ts
@@ -7,7 +7,7 @@ export class Engine extends RenderingLayer {
     readonly loop: Loop;
 
 
-    constructor(canvas: HTMLCanvasElement, width: number, height: number, pixelScale: number = 1, updateStyleSizeCallback?: UpdateStyleSizeCallback | null) {
+    constructor(canvas: HTMLCanvasElement, width: number, height: number, pixelScale?: number, updateStyleSizeCallback?: UpdateStyleSizeCallback | null) {
         super(canvas, width, height, pixelScale, updateStyleSizeCallback);
 
         this.loop = new Loop();
