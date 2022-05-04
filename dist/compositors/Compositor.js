ae7835803989640e9499fa992577dafb44a6f983
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-05-04 21:42:34 +0200

    Bug fixed

--- dist/compositors/Compositor.js
+++ dist/compositors/Compositor.js
@@ -59,7 +59,7 @@ export class Compositor {
         else {
             Shadow.clear(renderingLayer);
         }
-        ctx.moveTo(-t.origin.x * pxs, -t.origin.y * pxs);
+        ctx.translate(-t.origin.x * pxs, -t.origin.y * pxs);
         ctx.drawImage(source, 0, 0, this.width * pxs, this.height * pxs);
         renderingLayer.resetMatrix();
         ctx.globalAlpha = 1;
